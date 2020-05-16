4a0f2eb86d9c8e54ae5cd3a9cc6fd5d982501e26
jvaurof <jvaurof@gmail.com>
2020-05-16 16:24:59 -0300

    user options

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,55 +5,12 @@ import { useNavigation } from '@react-navigation/native';
 
 import MainButton from '../../components/MainButton'
 
+import userOptions from './userOptions';
+
 import {
   LineView,
   AnimatableTitle
 } from './styles';
-
-const userOptions = [
-  {
-    id: '1',
-    text: 'IMC',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'IMC',
-  },
-  {
-    id: '2',
-    text: 'Peso Ideal',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'PesoIdeal',
-  },
-  {
-    id: '3',
-    text: 'Peso Ajustado',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'Main',
-  },
-  {
-    id: '4',
-    text: 'RCQ',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'Main',
-  },
-  {
-    id: '5',
-    text: 'Peso Corrigido',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'Main',
-  },
-  {
-    id: '6',
-    text: 'Peso Corrigido',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'Main',
-  },
-  {
-    id: '7',
-    text: 'Peso Corrigido',
-    subText: 'Criança, Adulto e Idoso',
-    navigate: 'Main',
-  },
-]
 
 const Main = () => {
   const navigation = useNavigation();
